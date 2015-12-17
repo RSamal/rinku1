bd26c470ef40728b3f8b66f08962da41c1e4f58c
RSamal <samal.ramakanta@gmail.com>
2015-12-16 16:25:42 -0800

    updated files

--- scripts/centos-web.sh
+++ scripts/centos-web.sh
@@ -14,3 +14,5 @@ service httpd start
 yum install -y php php-cli  php-devel php-common php-mysql
 
 # Download the Web Content
+sudo -u vagrant wget -q "https://raw.githubusercontent.com/RSamal/rinku1/master/files/index.html"
+sudo -u vagrant wget -q "https://raw.githubusercontent.com/RSamal/rinku1/master/files/info.php"
